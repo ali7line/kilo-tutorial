11857035a7c73e646805c22e910d695f14b8b20e
ali7line <soltani.110@gmail.com>
2017-07-21 21:04:16 +0430

    enable raw mode

--- kilo.c
+++ kilo.c
@@ -1,6 +1,24 @@
 #include <unistd.h>
+#include <termios.h>
+
+
+void enableRawMode() {
+    struct termios raw;
+
+    tcgetattr(STDIN_FILENO, &raw);
+
+    raw.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL | IXON);
+    raw.c_oflag &= ~OPOST;
+    raw.c_lflag &= ~(ECHO | ECHONL | ICANON | ISIG | IEXTEN);
+    raw.c_cflag &= ~(CSIZE | PARENB);
+    raw.c_cflag |= CS8;
+
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
 
 int main() {
+    enableRawMode();
+
     char c;
     while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q' );
     return 0;
